7b4b1f0c0757ee1d07a695332e40c9329fe47c58
Daniel Seiner <51313652+dannyseiner@users.noreply.github.com>
2026-04-13 16:15:36 +0200

    [HERMES] deploy fix

--- hermes/next.config.ts
+++ hermes/next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   transpilePackages: ["@webtools/error-kit"],
+  outputFileTracingIncludes: {
+    "/*": ["./messages/**/*"],
+  },
 };
 
 export default nextConfig;
